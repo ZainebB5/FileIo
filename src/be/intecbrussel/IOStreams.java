2b42c9bef55b21961f19f42766421e189f61feea
Zaineb Bakkali <ZainebB@cursisten.intecbrussel.intra>
2022-06-23 11:54:02 +0200

    methode writeToFile added

--- src/be/intecbrussel/IOStreams.java
+++ src/be/intecbrussel/IOStreams.java
@@ -1,6 +1,12 @@
 package be.intecbrussel;
 
 import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class IOStreams {
     public static void main(String[] args) {
@@ -14,7 +20,24 @@ public class IOStreams {
     }
     //hulp methodes
     private static void writeToFile(){
-        BufferedWriter bufferedWriter = new BufferedWriter();
+    Path path = Paths.get("MyFolder/Secrets/secrets");
+//        //automatish afgeclosed als de lijncode tussen  haakjes na try
+//
+//        try (Writer writer = new FileWriter(path.toFile());
+//             BufferedWriter bufferedWriter = new BufferedWriter(writer);){
+//
+//            bufferedWriter.write("hello World 2");
+//            //bufferedWriter.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path)){
+            bufferedWriter.write("hello World 3");
+
+        } catch (IOException e) {
+         e.printStackTrace();
+      }
+
     };
     private static void readToFile(){};
 
